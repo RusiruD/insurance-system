d4172694756ace6270d46f4f48111478dff2862c
Rusiru <rdha868@aucklanduni.ac.nz>
2023-03-19 22:32:41 +1300

    insurance_system.java passes empty databse test and profile.java created

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -1,18 +1,41 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
+
 import nz.ac.auckland.se281.Main.PolicyType;
 
 public class InsuranceSystem {
+  ArrayList<String> names = new ArrayList<String>();
+  ArrayList<String> ages= new ArrayList<String>();
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
   }
 
   public void printDatabase() {
-    // TODO: Complete this method.
+      // checks if array list of names of clients is empty and if 
+      //so outputs a print statement saying the database has no profiles
+      if(names.isEmpty()){
+      
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0", "s", ".");  }
+      else{}
+        
+        
+
+        
   }
 
   public void createNewProfile(String userName, String age) {
-    // TODO: Complete this method.
+    
+    
+    
+     
+     
+      
+    
+    
+      
+      
+
   }
 
   public void loadProfile(String userName) {
