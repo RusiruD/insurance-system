394973066e4010ac1f99b239c8324046a193dc08
Rusiru <rdha868@aucklanduni.ac.nz>
2023-03-20 02:34:40 +1300

    fixed more style errors through the use of the google java format extension

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -12,100 +12,86 @@ public class InsuranceSystem {
   }
 
   public void printDatabase() {
-      // checks if array list of names of clients is empty and if 
-      //so outputs a print statement saying the database has no profiles
-      if(names.isEmpty()) {
-      
-        MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0", "s", ".");  
+    // checks if array list of names of clients is empty and if
+    // so outputs a print statement saying the database has no profiles
+    if (names.isEmpty()) {
+
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0", "s", ".");
+    } else {
+      // if the array list of names isnt empty it assigns a string variable to the size of the array
+      // list
+      String profileAmount = Integer.toString(names.size());
+
+      // checks if the size of the names array list is equal to 1 and if so prints the appropriate
+      // output statements as defined in the assignment document
+      if (names.size() == 1) {
+        MessageCli.PRINT_DB_POLICY_COUNT.printMessage(profileAmount, "", ":");
+        MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(
+            profileAmount, names.get(0), ages.get(0), " %s: %s, %s");
+
       }
-      else{
-          // if the array list of names isnt empty it assigns a string variable to the size of the array list 
-          String amount_of_names=Integer.toString(names.size());
-          
-           // checks if the size of the names array list is equal to 1 and if so prints the appropriate 
-           //output statements as defined in the assignment document
-          if(names.size()==1) {
-            MessageCli.PRINT_DB_POLICY_COUNT.printMessage(amount_of_names, "", ":");
-            MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(amount_of_names,names.get(0), ages.get(0)," %s: %s, %s");
-            
-          }
-          //if the amount of profiles is greater than 1 the names and ages are output one after each other depending on the position they are in the databse as
-          // they are output according to a strict format where it goes space rank colon space there name comma and space and there age 
-          else{
-            MessageCli.PRINT_DB_POLICY_COUNT.printMessage(amount_of_names, "s", ":");
-            for(int i=0; i<names.size(); i++) {
-              String rank=Integer.toString(i+1);
-              MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(rank, names.get(i), ages.get(i)," %s: %s, %s");
-  
-            }
-          }
-          
+      // if the amount of profiles is greater than 1 the names and ages are output one after each
+      // other depending on the position they are in the databse as
+      // they are output according to a strict format where it goes space rank colon space there
+      // name comma and space and there age
+      else {
+        MessageCli.PRINT_DB_POLICY_COUNT.printMessage(profileAmount, "s", ":");
+        for (int i = 0; i < names.size(); i++) {
+          String rank = Integer.toString(i + 1);
+          MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(
+              rank, names.get(i), ages.get(i), " %s: %s, %s");
         }
-       
+      }
     }
-  
-     
-    public void createNewProfile(String userName, String age) {
-
-      // formats the inputted userName into titlecase where the first letter is capitalised only
-      userName=(userName.substring(0,1).toUpperCase()+ userName.substring(1,userName.length()).toLowerCase());
-    
-      
-      
-      
-      // sets int_age to be a integer array equivalent to the string value age
-      int int_age =Integer.parseInt(age);
-      // checks if the age of the profile is greater than or equal to 0
-      // if the name is already contained in the database
-      // and if the username length is greater than or equal to 3 and if so adds the profile to the database
-      if (int_age>=0 && !names.contains(userName) && userName.length()>=3) {
-        names.add(userName);
-        ages.add(age);
-        
-        MessageCli.PROFILE_CREATED.printMessage(userName , age ,"New profile created for %s with age %s.");
-        //System.out.println("hdh");
-        
-        
-        //Profile =new Profile(userName, age);
-        
-       
-       
-        
-      }
-    //if the user name length is less than 3 characters an error message is printed
-      else if(userName.length()<3) {
-        MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(userName,"'%s' is an invalid username, it should be at least 3 characters long. No profile was"+ " created.");
-      }
-      
-      //if the age is less than 0 a error message is printed
-      else if(int_age<0) {
-        MessageCli.INVALID_AGE.printMessage(age,userName,"'%s' is an invalid age, please provide a positive whole number only. No profile was created"+ " for %s.");
-      }
-      
-        //if the username inputted is already in the database an error message is printed
-      else if(names.contains(userName)) {
-        MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(userName,"Usernames must be unique. No profile was created for '%s'.");
-      }
-      
-      
-
   }
 
+  public void createNewProfile(String userName, String age) {
 
+    // formats the inputted userName into titlecase where the first letter is capitalised only
+    userName =
+        (userName.substring(0, 1).toUpperCase()
+            + userName.substring(1, userName.length()).toLowerCase());
 
-  
-    
-    
-    
-     
-     
-      
-    
-    
-      
-      
+    // sets int_age to be a integer array equivalent to the string value age
+    int ageInteger = Integer.parseInt(age);
+    // checks if the age of the profile is greater than or equal to 0
+    // if the name is already contained in the database
+    // and if the username length is greater than or equal to 3 and if so adds the profile to the
+    // database
+    if (ageInteger >= 0 && !names.contains(userName) && userName.length() >= 3) {
+      names.add(userName);
+      ages.add(age);
 
-  
+      MessageCli.PROFILE_CREATED.printMessage(
+          userName, age, "New profile created for %s with age %s.");
+      // System.out.println("hdh");
+
+      // Profile =new Profile(userName, age);
+
+    }
+    // if the user name length is less than 3 characters an error message is printed
+    else if (userName.length() < 3) {
+      MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(
+          userName,
+          "'%s' is an invalid username, it should be at least 3 characters long. No profile was"
+              + " created.");
+    }
+
+    // if the age is less than 0 a error message is printed
+    else if (ageInteger < 0) {
+      MessageCli.INVALID_AGE.printMessage(
+          age,
+          userName,
+          "'%s' is an invalid age, please provide a positive whole number only. No profile was"
+              + " created for %s.");
+    }
+
+    // if the username inputted is already in the database an error message is printed
+    else if (names.contains(userName)) {
+      MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(
+          userName, "Usernames must be unique. No profile was created for '%s'.");
+    }
+  }
 
   public void loadProfile(String userName) {
     // TODO: Complete this method.
